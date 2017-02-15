8d9b1e4e42529128bca0379cdfe5e9306d9d87ec
Nam Sy Vo <vosynam@gmail.com>
2017-02-14 20:03:24 -0600

    Some changes.

--- PeptidePrediction.py
+++ PeptidePrediction.py
@@ -80,6 +80,8 @@ def predict_hla_bound_peptides():
 
 	hla_num = 0
 	for hla_name, hla_pssm in PSSM_HLA.iteritems():
+		if hla_name not in ["#HLA-A0201", "#HLA-A2902", "#HLA-B0702", "#HLA-B4403", "#HLA-C0702"]:
+			continue
 		top_aa = []
 		top_aa_set = set()
 		path_num = 1
@@ -98,6 +100,7 @@ def predict_hla_bound_peptides():
 				top_aa_set.add(k)
 			top_aa.append(top_name)
 			path_num *= (i-1)
+		print "# of paths", path_num
 
 		cand_pep_seq = {}
 		for k, v in pep_seq_set.iteritems():
@@ -110,7 +113,7 @@ def predict_hla_bound_peptides():
 		if len(cand_pep_seq) == 0:
 			continue
 
-		f = open(os.path.join(dir_name + str(top_num), str(k_mer) + "-mer_log_" + str(k_mass) + ".txt"), "a")
+		f = open(os.path.join(result_path + str(top_num), str(k_mer) + "-mer_log_" + str(k_mass) + ".txt"), "a")
 		hla_num += 1
 		f.write(str(hla_num) + "\t" + str(hla_name) + "\t" + str(top_aa) + "\n")
 		print "hla", hla_num, hla_name
@@ -120,7 +123,11 @@ def predict_hla_bound_peptides():
 		f.close()
 
 		bound_pep_seq = {}
+		count = 0
 		for t in itertools.product(*top_aa):
+			count += 1
+			if count % 100000 == 0:
+				print count
 			for k, v in cand_pep_seq.iteritems():
 				bound_pep = []
 				for pep in v:
@@ -133,7 +140,7 @@ def predict_hla_bound_peptides():
 		if len(bound_pep_seq) == 0:
 			continue
 		for k, v in bound_pep_seq.iteritems():
-			f = open(os.path.join(dir_name + str(top_num), str(k_mer) + "-mer_" + str(k) + "-mass_bound.txt"), "a")
+			f = open(os.path.join(result_path + str(top_num), str(k_mer) + "-mer_" + str(k) + "-mass_bound.txt"), "a")
 			for bound_pep in v:
 				for pep in bound_pep:
 					f.write(pep[0] + "\t" + pep[1] + "\t" + ''.join(pep[2]) + "\n")
@@ -145,7 +152,6 @@ if __name__ == "__main__":
 
 	ppm = 10.0
 	k_mer = 9
-	dir_name = "next_predicted_peptides_bound_top"
 
 	print "reading input data..."
 	uni_pep_mass = parseMGF(sys.argv[1])
@@ -160,27 +166,29 @@ if __name__ == "__main__":
 	f.close()
 
 	pssm_path = sys.argv[3]
+	result_path = sys.argv[6]
 	top_num = int(sys.argv[4])
 	k_mass = int(sys.argv[5])
 
-	if not os.path.exists(dir_name + str(top_num)):
-		os.makedirs(dir_name + str(top_num))
+	if not os.path.exists(result_path + str(top_num)):
+		os.makedirs(result_path + str(top_num))
 
 	pep_seq, pep_mass = {}, {}
 	# Test
-	'''
 	seq_list = ["KIVGAGPGA","LGGSGSGLR","AVATEAPNL","IGIAPLAQL","GLLGTLVQL","DGGNESDPM","ALASHLIEA","AIVDKVPSV","SLLDKIIGA","SLLGGNIRL","ALLDSAHLL","SLLEKSLGL","AVLTELRAV","TLIEDILGV","LSAEKIQAL","KLIANNTTV","ISRALVTTL","SLGLPQDVPG","SLFPGKLEV"]
 	mz_list = ["385.23495","402.22269","443.23782","448.28342","457.28906","461.16895","462.76172","464.27966","465.28659","471.7916","476.77765","480.29266","486.29782","486.78546","486.79044","487.28766","487.30121","491.76447","495.28641"]
 	mass_list = ["769.46263","803.4381","885.46837","895.55956","913.57085","921.33061","924.51616","927.55205","929.5659","942.57591","952.54802","959.57805","971.58837","972.56365","972.57359","973.56804","973.59514","982.52165","989.56554"]
 	'''
 	seq_list = ["VLLGKVYVV", "VMAPRTLVL", "SLAEVLQQL", "RPTSRLATL", "NLAENISRV", "LIYRAVVLA", "SLAQYLINV", "KVANIILSY", "VILSLILHL", "VILSLILHL", "VILPLISRL", "VMITKLVEV", "MLLAVIFDL", "KMMDVTVTI", "ALFPERITV", "LVIRVLLLL", "EALGNVSKKL", "WNTLGLFTL", "VTDDYNHLV", "ISDDYNHLV", "VTADSRVVGAL", "MALGTHTMEV", "RLIESLFTI", "PmGLSFSLLL", "AmQGAKLYSI", "ELKPFIEEL", "KLQEFLQTL", "KLFEKVKEV", "RNADVFLKY", "LAKLDVKILL", "VTYSYFQKV", "VTYSYFQKV", "KLLSKFYEL", "QILSGRKPEL", "LTWYRAPEI", "KLFNEFIQL", "IIVVTTMILY", "RLFADILNDV", "WKTDQVPFSV", "TIVAIEVKKIL", "FLASESLIKQI", "RVDPNGSRYLL", "FFGLLWYAVAY", "FFGLLWYAVAY", "LLMEKEDYHIS"]
-	mz_list = ["495.32471", "500.30469", "500.78897", "507.8013", "508.28049", "509.32669", "510.78931", "510.80905", "510.8446", "510.84479", "512.35028", "516.31384", "517.7998", "519.27264", "523.30267", "526.3847", "529.81024", "532.7915", "538.25623", "538.25641", "544.31195", "545.25781", "546.32758", "547.30139", "549.28186", "559.31244", "560.32416", "560.34253", "563.30664", "375.92252", "567.79523", "567.79541", "570.83472", "570.83679", "574.80975", "576.3244", "583.3537", "588.32593", "603.81152", "409.60849", "624.86481", "645.35419", "675.34674", "675.34698", "689.34174"]
+	mz_list = ["495.32471", "500.30469", "500.78897", "507.8013", "508.28049", "509.32669", "510.78931", "510.80905", "510.8446", "510.84479", "512.35028", "516.31384", "517.7998", "519.27264", "523.30267", "526.3847", "529.81024", "532.7915", "538.25623", "538.25641", "544.31195", "545.25781", "546.32758", "547.30139", "549.28186", "559.31244", "560.32416", "560.34253", "563.30664", "375.92252", "567.79523", "567.79541", "570.83472", "570.83679", "574.80975", "576.3244", "583.3537", "588.32593", "603.81152", "609.60849", "624.86481", "645.35419", "675.34674", "675.34698", "689.34174"]
 	mass_list = ["989.64214", "999.6021", "1000.57067", "1014.59532", "1015.5537", "1017.6461", "1020.57134", "1020.61083", "1020.68193", "1020.6823", "1023.69328", "1031.62041", "1034.59233", "1037.53801", "1045.59807", "1051.76213", "1058.61321", "1064.57573", "1075.50517", "1075.50554", "1087.61662", "1089.50835", "1091.64787", "1093.59551", "1097.55644", "1117.6176", "1119.64104", "1119.67778", "1125.606", "1125.75299", "1134.58318", "1134.58354", "1140.66216", "1140.66631", "1148.61223", "1151.64153", "1165.70012", "1175.64458", "1206.61577", "1226.81092", "1248.72234", "1289.7011", "1349.6862", "1349.68669", "1377.6762"]
+	'''
 	for i in range(k_mass, k_mass + 1):
+	#for i in range(len(seq_list)):
 		pep_seq[mz_list[i]] = [seq_list[i]]
 		pep_mass[mz_list[i]] = [mass_list[i]]
 
-		f = open(os.path.join(dir_name + str(top_num), str(k_mer) + "-mer_" + str(mz_list[i]) + "-mass_predicted.txt"), "w")
+		f = open(os.path.join(result_path + str(top_num), str(k_mer) + "-mer_" + str(mz_list[i]) + "-mass_predicted.txt"), "w")
 		f.write("predicted_peptides\taa_mass\n")
 		for j in range(len(pep_seq[mz_list[i]])):
 			f.write(pep_seq[mz_list[i]][j] + "\t" + pep_mass[mz_list[i]][j] + "\n")
@@ -205,7 +213,7 @@ if __name__ == "__main__":
 		pep_mass[pm] = pep_mass_list
 		print pm, len(pep_seq[pm])
 
-		f = open(os.path.join(dir_name + str(top_num), str(k_mer) + "-mer_" + str(pm) + "-mass_predicted.txt"), "w")
+		f = open(os.path.join(result_path + str(top_num), str(k_mer) + "-mer_" + str(pm) + "-mass_predicted.txt"), "w")
 		f.write("peptides\taa_mass\n")
 		for i in range(len(pep_seq[pm])):
 			f.write(pep_seq[pm][i] + "\t" + pep_mass[pm][i] + "\n")
---
+++ hla_binding_analysis.py
@@ -0,0 +1,126 @@
+import sys
+import os
+
+import PeptidePrediction
+
+hla_binding = {}
+seq_mass = {}
+for (dirpath, dirnames, filenames) in os.walk(os.path.join(sys.argv[1])):
+	for filename in filenames:
+		if ".txt" not in filename:
+			continue
+		f = open(os.path.join(dirpath, filename))
+		for line in f:
+			tokens = line.strip().split()
+			if tokens[2] not in hla_binding:
+				hla_binding[tokens[2]] = set()
+			if tokens[2] not in seq_mass:
+				seq_mass[tokens[2]] = ""
+			hla_binding[tokens[2]].add(tokens[1])
+			seq_mass[tokens[2]] = filename
+		f.close()
+
+'''
+hla_alleles = ["A0201", "A2902", "B0702", "B4403", "C0702"]
+hla_num = {}
+print "#peptides", len(hla_binding)
+#for k, v in hla_binding.iteritems():
+#	print k, seq_mass[k], len(v), "\t".join(v)
+for k, v in hla_binding.iteritems():
+	for hla in hla_alleles:
+		for t in v:
+			if hla in t:
+				print k, t, seq_mass[k]
+'''
+aa_names, pssm_hla_1, pssm_hla_2 = PeptidePrediction.getPSSM(sys.argv[2])
+pssm_hla = pssm_hla_1.copy()
+pssm_hla.update(pssm_hla_2)
+#print "# of HLA alleles:", len(pssm_hla)
+
+from numpy import prod
+import itertools
+
+top_num = 5
+hla_path_score = {}
+for hla, pssm in pssm_hla.iteritems():
+		top_score = []
+		path_num = 1
+		for pos_val in pssm:
+			pssm_dict = {}
+			for i in range(len(pos_val)):
+				pssm_dict[aa_names[i]] = pos_val[i]
+			top_val, top_name = [], []
+			i = 0
+			for k in sorted(pssm_dict, key=pssm_dict.get, reverse=True):
+				i += 1
+				if i > top_num: #take top_num AAs
+					break
+				top_val.append(pssm_dict[k])
+				top_name.append(k)
+			top_score.append(top_val)
+			path_num *= (i-1)
+
+		scores = []
+		for t in itertools.product(*top_score):
+			scores.append(prod(t))
+		hla_path_score[hla] = scores
+
+		print "Finish", hla
+
+print len(hla_path_score)
+
+check_hla = ["#HLA-A0201", "#HLA-A2902", "#HLA-B0702", "#HLA-B4403", "#HLA-C0702"]
+for pep, hla_list in hla_binding.iteritems():
+	'''
+	score = []
+	min_top5_score = []
+	for hla in check_hla:
+		s = 0
+		for i in range(len(pep)):
+			for j in range(len(aa_names)):
+				if pep[i] == aa_names[j]:
+					s += pssm_hla[hla][i][j]
+					break
+		score.append(s)
+		min_s = 0
+		for i in range(len(pep)):
+			tmp = sorted(pssm_hla[hla][i], reverse=True)
+			min_s += tmp[4]
+		min_top5_score.append(min_s)
+	print pep + "\t",
+	for i in range(len(score)):
+		print "\t" + str(score[i] - min_top5_score[i]),
+	print
+	'''
+	'''
+	for hla in hla_list:
+		s = 0
+		for i in range(len(pep)):
+			for j in range(len(aa_names)):
+				if pep[i] == aa_names[j]:
+					s += pssm_hla[hla][i][j]
+					break
+
+		min_s = 0
+		for i in range(len(pep)):
+			tmp = sorted(pssm_hla[hla][i], reverse=True)
+			min_s += tmp[4]
+
+		max_s = 0
+		for i in range(len(pep)):
+			max_s += max(pssm_hla[hla][i])
+
+		print pep + "\t" + hla + "\t" + seq_mass[pep] + "\t" + str(s) + "\t" + str(min_s) + "\t" + str(max_s)
+	'''
+
+	for hla in hla_list:
+		s = 0
+		for i in range(len(pep)):
+			for j in range(len(aa_names)):
+				if pep[i] == aa_names[j]:
+					s += pssm_hla[hla][i][j]
+					break
+
+		for score in sorted(hla_path_score[hla], reverse=True):
+			if score < s:
+				print pep + "\t" + hla + "\t" + seq_mass[pep] + "\t" + str(s) + "\t" + str(score)
